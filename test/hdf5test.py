510e5f5ebfbf4d916b45647dda7ced0a547e810d
Albert Thuswaldner <albert.thuswaldner@gmail.com>
2013-05-11 13:25:48 +0200

    updated test script to check all variable types supported by pytables

--- test/hdf5test.py
+++ test/hdf5test.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python
-# Last modified Thu May  9 22:28:50 2013 on havoc
-# update count: 12
+# Last modified Sat May 11 13:19:48 2013 on havoc
+# update count: 47
 
 from hdf5io import *
+from numpy import array 
 
-# test function
-def hdf5test():
-    # Create some test variables
-    var = 1
-    varstring='test string'
-    vararray=[1, 32, 3]
+# - Test supported types
+varint = 1
+varfloat = 3.14159
+#varstring='test string'
+varcomplex=(1-4j)
+ivarlist=[1, 32, 3]
+#vartuple = (1, 2, 'test string')
+vararray = array([1,2,3])
+#vararrays = array([(1,2,3,'a'), (2,3,5,'b')])
+vararrayc = array( [ [1,2], [3,4] ], dtype=complex )
 
-    # Save to file
-    hdf5save("test.h5")
+# Save to file
+hdf5save("test.h5")
 
-    # Show file content 
-    hdf5ls("test.h5")
+# Show file content 
+hdf5ls("test.h5")
 
-    # Append a variable
-    var2=67.3
-    hdf5save("test.h5",'var2',"/",'a')
+# - Append a variable
+var2=67.3
+hdf5save("+test.h5",'var2',"/")
 
-    # Show file content again 
-    hdf5ls("test.h5")
+# Show file content again 
+hdf5ls("test.h5")
 
-    
+# - Add group
+var2=67.3
+hdf5save("+test.h5",'var2',"/test")
+
+# Show file content again 
+hdf5ls("test.h5")
+ 
