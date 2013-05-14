8bfe3f4eb8540eef9884a14b7c5db3b85a26d45c
Albert Thuswaldner <albert.thuswaldner@gmail.com>
2013-05-15 01:06:04 +0200

    - re-organized test file into functions - added supported variable types for testing - added test to load all variables.

--- test/hdf5test.py
+++ test/hdf5test.py
@@ -1,38 +1,63 @@
-#!/usr/bin/env python
-# Last modified Sat May 11 13:19:48 2013 on havoc
-# update count: 47
+#!/usr/bin/env python3
+# encoding: utf-8
+# Last modified Wed May 15 01:03:06 2013 on havoc
+# update count: 93
 
 from hdf5io import *
 from numpy import array 
 
-# - Test supported types
-varint = 1
-varfloat = 3.14159
-#varstring='test string'
-varcomplex=(1-4j)
-ivarlist=[1, 32, 3]
-#vartuple = (1, 2, 'test string')
-vararray = array([1,2,3])
-#vararrays = array([(1,2,3,'a'), (2,3,5,'b')])
-vararrayc = array( [ [1,2], [3,4] ], dtype=complex )
+def testsavetypes():
+    """ Test saveing supported variable types """
+    varint = 1
+    varfloat = 3.14159
+    varstring="test string"
+    varcomplex=(1-4j)
+    varlist=[1, 32, 3]
+    varlists=[1, 32, 'test string']
+    vartuple = (1, 2, 3)
+    vartuples = (1, 2, 'test string')
+    vararray = array([1,2,3])
+    #vararrays = array([(1,2,3,'a'), (2,3,5,'b')])
+    vararrayc = array( [ [1,2], [3,4] ], dtype=complex )
 
-# Save to file
-hdf5save("test.h5")
+    # Save to file
+    hdf5save("test.h5")
+    return locals()
 
-# Show file content 
-hdf5ls("test.h5")
+def showfile():
+    """" Show file content """ 
+    hdf5ls("test.h5")
 
-# - Append a variable
-var2=67.3
-hdf5save("+test.h5",'var2',"/")
+def testappend(): 
+    """ Test to append variable """
+    var2=67.3
+    hdf5save("+test.h5",'var2',"/")
 
-# Show file content again 
-hdf5ls("test.h5")
+def testaddgroup():
+    """ Test to add a group """
+    var2=67.3
+    hdf5save("+test.h5",'var2',"/test")
 
-# - Add group
-var2=67.3
-hdf5save("+test.h5",'var2',"/test")
+def testloadtypes():
+    """ Test loading supported variable types """
+    hdf5load("test.h5")
+    return locals()
 
-# Show file content again 
-hdf5ls("test.h5")
- 
+def main():
+    """ Main test function """
+    savedict = testsavetypes()
+    showfile()
+    #print(savedict)
+    loaddict = testloadtypes()
+    #print(loaddict)
+    if savedict == loaddict:
+        print("load/save success!")
+    else:
+        print("load/save FAILURE!")
+    testappend()
+    showfile()
+    testaddgroup()
+    showfile()
+
+if __name__ == '__main__':
+    main()
